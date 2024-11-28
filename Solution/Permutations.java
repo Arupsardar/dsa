9f69d81ee3d6b8e09737cf7490cf07fd0456834a
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2024-11-28 22:17:06 +0530

    46. Permutations Approach - 2 by arup

--- Solution/Permutations.java
+++ Solution/Permutations.java
@@ -1,13 +1,38 @@
 package Solution;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Permutations {
 
+    /*
+     * Approach - 1
+     * TC -(O(!n))
+     * SC -O(n)
+     */
+
     public void resusion(int[] nums,List<List<Integer>> ans, List<Integer> ds, boolean [] feq){
         if(ds.size()==nums.length){
             ans.add(new ArrayList<>(ds));
+            return;
+        }
+
+        for(int i=0;i<nums.length;i++){
+            if(!feq[i]){
+                feq[i]=true;
+                ds.add(nums[i]);
+                resusion(nums, ans, ds, feq);
+                feq[i]=false;
+                ds.remove(ds.size()-1);
+            }
+        }
+    }
+
+    public void resusion2(int[] nums,List<List<Integer>> ans, int ind){
+        if(ind==nums.length){
+            
+            ans.add(Arrays.stream(int).boxed().toList());
             return;
         }
 
