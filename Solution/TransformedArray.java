6db5b1a716da72dd2e9ff89e81de3c8c7d1cbb45
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2024-12-08 21:38:57 +0530

    3379. Transformed Array by arup

--- Solution/TransformedArray.java
+++ Solution/TransformedArray.java
@@ -1,23 +1,13 @@
-import java.util.*;
+package Solution;
 
-
-
-public class Main {
-    
-    public static void main(String[] args) {
-        System.out.println("hello");
-        int [] arr ={-10,-10};
-        constructTransformedArray(arr);
-    }
-
-    public static int[] constructTransformedArray(int[] nums) {
+public class TransformedArray {
+    public int[] constructTransformedArray(int[] nums) {
         int n =nums.length;
         int[] rest = new int[n];
         for(int i=0;i<n;i++){
              if(nums[i]>0){
                 rest[i]=nums[(i+nums[i])%n];
              }else{
-                
                 rest[i]=nums[(i-(Math.abs(nums[i])%n)+n)%n];
              }
              
@@ -25,8 +15,4 @@ public class Main {
         return rest;
         
     }
-
-    
-
-    
 }
