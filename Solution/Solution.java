0162fa8cf8199622e2a10d6979e3a0ad130390d8
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2024-12-29 19:19:08 +0530

    3402. Minimum Operations to Make Columns Strictly Increasing

--- Solution/Solution.java
+++ Solution/Solution.java
@@ -1,5 +1,7 @@
 package Solution;
 
+import java.util.*;
+
 public class Solution {
     /*
      * 3396. Minimum Number of Operations to Make Elements in Array Distinct
@@ -24,5 +26,48 @@ public class Solution {
             
         }
         return count;
+    }
+    /*
+     3402. Minimum Operations to Make Columns Strictly Increasing
+        Solved
+        Easy
+        Companies
+        Hint
+        You are given a m x n matrix grid consisting of non-negative integers.
+
+        In one operation, you can increment the value of any grid[i][j] by 1.
+
+        Return the minimum number of operations needed to make all columns of grid strictly increasing.
+
+        
+
+        Example 1:
+
+        Input: grid = [[3,2],[1,3],[3,4],[0,1]]
+
+        Output: 15
+
+        Explanation:
+
+        To make the 0th column strictly increasing, we can apply 3 operations on grid[1][0], 2 operations on grid[2][0], and 6 operations on grid[3][0].
+        To make the 1st column strictly increasing, we can apply 4 operations on grid[3][1].
+     */
+
+
+    public int minimumOperations(int[][] grid) {
+        int count=0;
+        int n=grid.length;
+        int m =grid[0].length;
+        for(int j=0;j<m;j++){
+            for(int i=1;i<n;i++){
+                if(grid[i][j]<= grid[i-1][j]){
+                    count =count+(grid[i-1][j]-grid[i][j])+1;
+                    grid[i][j]=grid[i-1][j]+1;
+                }
+                
+            }
+        }
+        return count;
+        
     }
 }
