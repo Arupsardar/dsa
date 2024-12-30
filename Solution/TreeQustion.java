ed51272878d45d9053f28d9842be1b48b4e15ea3
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2024-12-30 18:55:40 +0530

    2583. Kth Largest Sum in a Binary Tree by arup

--- Solution/TreeQustion.java
+++ Solution/TreeQustion.java
@@ -1,6 +1,7 @@
 package Solution;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
@@ -8,29 +9,38 @@ import strucher.TreeNode;
 
 public class TreeQustion {
     public long kthLargestLevelSum(TreeNode root, int k) {
-        List<Integer> arr =new ArrayList<>();
-        levelwisesum(root,0,arr);
-        long level=-1;
-        int maxval=arr.get(0);
-        for(int i=1;i<arr.size();i++){
-            if(arr.get(i)>maxval){
-                level =i;
-                maxval=arr.get(i);
-            }
+        List<Long> levelSums = new ArrayList<>();
+        levelWiseSum(root, 0, levelSums);
+
+        // Sort the level sums in descending order
+        Collections.sort(levelSums, Collections.reverseOrder());
+
+        // If k is greater than the number of levels, return -1
+        if (k > levelSums.size()) {
+            return -1;
         }
-        return level;
+
+        // Return the k-th largest sum
+        return levelSums.get(k - 1);
 
         
     }
 
-    public void levelwisesum(TreeNode root,int level,List <Integer> arr){
-         if(root==null){
+    public void levelWiseSum(TreeNode root, int level, List<Long> levelSums) {
+        if (root == null) {
             return;
-         }
-         int val =arr.get(level);
-         val =val+root.val;
-         arr.add(level,val);
-         levelwisesum(root.left,level+1,arr);
-         levelwisesum(root.right,level+1,arr);
+        }
+
+        // Expand the list if the current level does not exist in the list
+        if (level == levelSums.size()) {
+            levelSums.add(0L);
+        }
+
+        // Update the sum for the current level
+        levelSums.set(level, levelSums.get(level) + root.val);
+
+        // Recur for left and right subtrees
+        levelWiseSum(root.left, level + 1, levelSums);
+        levelWiseSum(root.right, level + 1, levelSums);
     }
 }
