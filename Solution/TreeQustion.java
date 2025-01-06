d80964133cba5a6c1e2d8da08adeb14eb993d4a5
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-01-06 21:45:36 +0530

    100. Same Tree by arup

--- Solution/TreeQustion.java
+++ Solution/TreeQustion.java
@@ -165,4 +165,29 @@ public class TreeQustion {
  
  
      }
+
+
+     /*
+      100. Same Tree
+
+        Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+
+        Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+      */
+        public boolean isSameTree(TreeNode p, TreeNode q) {
+           return isSameTree2(p,q);
+        }
+    
+        public boolean isSameTree2(TreeNode p, TreeNode q) {
+            if(p==null && q==null){
+                return true;
+            }
+            if(p==null && q  !=null || q==null && p!=null){
+                return false;
+            }
+            if(q.val !=p.val){
+                return false;
+            }
+            return isSameTree2(p.left,q.left)&& isSameTree2(p.right,q.right);
+        }
 }
