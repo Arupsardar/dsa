d17dee2bafab2f6939b4a30a991a3c3b74dcc603
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-01-12 21:23:45 +0530

    3417. Zigzag Grid Traversal With Skip by arup

--- Solution/Solution.java
+++ Solution/Solution.java
@@ -163,4 +163,54 @@ public class Solution {
     private long lcm(long a, long b) {
         return a * (b / gcd((int) a, (int) b));
     }
+
+    /*
+     3417. Zigzag Grid Traversal With Skip
+
+        You are given an m x n 2D array grid of positive integers.
+
+        Your task is to traverse grid in a zigzag pattern while skipping every alternate cell.
+
+        Zigzag pattern traversal is defined as following the below actions:
+
+        Start at the top-left cell (0, 0).
+        Move right within a row until the end of the row is reached.
+        Drop down to the next row, then traverse left until the beginning of the row is reached.
+        Continue alternating between right and left traversal until every row has been traversed.
+        Note that you must skip every alternate cell during the traversal.
+
+        Return an array of integers result containing, in order, the value of the cells visited during the zigzag traversal with skips.
+     */
+
+    public List<Integer> zigzagTraversal(int[][] grid) {
+        List<Integer> result = new ArrayList<>();
+         int m = grid.length;    // Number of rows
+         int n = grid[0].length; // Number of columns
+         boolean leftToRight = true;
+         int cellCount = 0;      // Global cell count for skip logic
+ 
+         for (int i = 0; i < m; i++) {
+             if (leftToRight) {
+                 // Traverse left to right
+                 for (int j = 0; j < n; j++) {
+                     if (cellCount % 2 == 0) {
+                         result.add(grid[i][j]);
+                     }
+                     cellCount++;
+                 }
+             } else {
+                 // Traverse right to left
+                 for (int j = n - 1; j >= 0; j--) {
+                     if (cellCount % 2 == 0) {
+                         result.add(grid[i][j]);
+                     }
+                     cellCount++;
+                 }
+             }
+             // Switch direction
+             leftToRight = !leftToRight;
+         }
+ 
+         return result;
+     }
 }
