25e022a6fa17678240e2fd96e4e0c861ad12c219
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-01-13 21:42:23 +0530

    111. Minimum Depth of Binary Tree by arup

--- Solution/TreeQustion.java
+++ Solution/TreeQustion.java
@@ -364,4 +364,27 @@ public class TreeQustion {
         }
         return new int[]{validNodeCount,sum,count};
     }
+
+    /*
+     111. Minimum Depth of Binary Tree
+
+        Given a binary tree, find its minimum depth.
+
+        The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+
+        Note: A leaf is a node with no children.
+     */
+
+    public int minDepth(TreeNode root) {
+        if(root==null){
+           return 0;
+        }
+        if (root.left == null) {
+             return 1 + minDepth(root.right);
+         }
+         if (root.right == null) {
+             return 1 + minDepth(root.left);
+         }
+        return 1+Math.min(minDepth(root.left),minDepth(root.right));
+     }
 }
