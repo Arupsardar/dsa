375319545c23afd7f28d776d550fe2e653e0c031
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-01-23 22:47:04 +0530

    129. Sum Root to Leaf Numbers Graph

--- Solution/TreeQustion.java
+++ Solution/TreeQustion.java
@@ -389,4 +389,35 @@ public class TreeQustion {
          
         return 1+Math.min(minDepth(root.left),minDepth(root.right));
      }
+
+     /*
+      129. Sum Root to Leaf Numbers
+
+        You are given the root of a binary tree containing digits from 0 to 9 only.
+
+        Each root-to-leaf path in the tree represents a number.
+
+        For example, the root-to-leaf path 1 -> 2 -> 3 represents the number 123.
+        Return the total sum of all root-to-leaf numbers. Test cases are generated so that the answer will fit in a 32-bit integer.
+
+        A leaf node is a node with no children.
+      */
+
+
+     public int sumNumbers(TreeNode root) {
+        return sumNumbers(root,0);
+    }
+
+    public int sumNumbers(TreeNode root,int num) {
+        if(root==null){
+            return 0;
+        }
+        num =num*10+root.val;
+        if(root.left ==null && root.right ==null){
+            return num;
+        }
+        int left =sumNumbers(root.left,num);
+        int right =sumNumbers(root.right,num);
+        return left+right;
+    }
 }
