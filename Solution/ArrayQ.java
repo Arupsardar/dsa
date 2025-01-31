d4a1a5052fbdc60bd2f54c8be73e75e5ee052a6d
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-01-31 22:24:56 +0530

    3074. Apple Redistribution into Boxes arup

--- Solution/ArrayQ.java
+++ Solution/ArrayQ.java
@@ -64,4 +64,34 @@ public class ArrayQ {
         }
         
     }
+
+    /*
+     3074. Apple Redistribution into Boxes
+
+        You are given an array apple of size n and an array capacity of size m.
+
+        There are n packs where the ith pack contains apple[i] apples. There are m boxes as well, and the ith box has a capacity of capacity[i] apples.
+
+        Return the minimum number of boxes you need to select to redistribute these n packs of apples into boxes.
+
+        Note that, apples from the same pack can be distributed into different boxes.
+     */
+
+    public int minimumBoxes(int[] apple, int[] capacity) {
+        int total =0;
+        for(int i=0;i<apple.length;i++){
+            total +=apple[i];
+        }
+        Arrays.sort(capacity);
+        int tota=0;
+        int count =0;
+        for(int i=capacity.length-1;i>=0;i--){
+            tota =tota+capacity[i];
+            count++;
+            if(tota >=total){
+                break;
+            }
+        }
+        return count;
+    }
 }
