0633700d05d8669e0872199fbcb45e0244ed4f5a
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-02-05 21:48:20 +0530

    45. Jump Game II Arup

--- Solution/Greedy.java
+++ Solution/Greedy.java
@@ -19,4 +19,48 @@ public class Greedy {
         }
         return true;
     }
+
+    
+    /*
+     45. Jump Game II
+
+        You are given a 0-indexed array of integers nums of length n. You are initially positioned at nums[0].
+
+        Each element nums[i] represents the maximum length of a forward jump from index i. In other words, if you are at nums[i], you can jump to any nums[i + j] where:
+
+        0 <= j <= nums[i] and
+        i + j < n
+        Return the minimum number of jumps to reach nums[n - 1]. The test cases are generated such that you can reach nums[n - 1].
+     */
+
+    public int jumpboot(int[] nums) {
+        return jump(nums,0,0);
+     }
+ 
+     public int jump(int[] nums,int jump,int count) {
+         if(jump>=nums.length-1){
+             return count;
+         }
+         int min=Integer.MAX_VALUE;
+         for(int i=1;i<=nums[jump];i++){
+             int num= jump(nums,jump+i,count+1);
+             min= Math.min(min,num);
+         }
+         return min;
+     }
+
+
+     public int jump(int[] nums) {
+        int jump =0;
+        int l =0;
+        int r=0;
+        for(int i=0;i<nums.length-1;i++){
+            l=Math.max(l,i+nums[i]);
+            if(i==r){
+              jump++;
+              r=l;
+            }
+        }
+        return jump;
+     }
 }
