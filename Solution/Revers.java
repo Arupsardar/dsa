faefcda6b2a468fba4b92488c71c05ac77b2481b
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-02-07 22:25:00 +0530

    541. Reverse String II by aru by aruo

--- Solution/Revers.java
+++ Solution/Revers.java
@@ -45,4 +45,26 @@ public class Revers {
 
         
     }
+
+    /*
+     541. Reverse String II
+
+        Given a string s and an integer k, reverse the first k characters for every 2k characters counting from the start of the string.
+
+        If there are fewer than k characters left, reverse all of them. If there are less than 2k but greater than or equal to k characters, then reverse the first k characters and leave the other as original.
+     */
+
+    public String reverseStr(String s, int k) {
+        char [] arr =s.toCharArray();
+        for(int j=0;j<s.length();j=j+2*k){
+            int end = Math.min(j + k - 1, arr.length - 1); 
+            for (int i = 0; i < (end - j + 1) / 2; i++) {
+                    char temp = arr[j + i];
+                    arr[j + i] = arr[end - i];
+                    arr[end - i] = temp;
+                }
+        }
+        
+        return new String(arr);
+    }
 }
