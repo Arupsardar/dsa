33e3808f0dc094953fc6c815912f68a49ab21841
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-02-17 22:16:17 +0530

    160. Intersection of Two Linked Lists  Arup

--- Main.java
+++ Main.java
@@ -9,38 +9,7 @@ import Concurrency.menory.MyRunable;
 public class Main {
 
     public static void main(String[] args) {
-        Runnable runnable1 = new MyRunable();  // Correct class name
-        Runnable runnable2s = new MyRunable();  // Correct class name
-
-        Thread thread1 = new Thread(runnable1, "Thread 1");
-        Thread thread2 = new Thread(runnable2s, "Thread 2");
         
-        thread1.start();
-        thread2.start();
+
     }
-
-
-    public long maxWeight(int[] pizzas) {
-         long ans =0;
-         int n=pizzas.length;
-         boolean [] array =new boolean[n];
-
-        
-    }
-
-    public long maxWeight(int[] pizzas,int day,boolean [] array) {
-        long ans =0;
-        
-       
-   }
-    
-    
-
-    
-
-    
-
-    
-
-    
 }
--- Solution/Solution.java
+++ Solution/Solution.java
@@ -2,6 +2,7 @@ package Solution;
 
 import java.util.*;
 
+import strucher.ListNode;
 import strucher.TreeNode;
 
 public class Solution {
@@ -630,6 +631,46 @@ public class Solution {
        }
        return false;
    }
+
+   /*
+    160. Intersection of Two Linked Lists
+
+        Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect. If the two linked lists have no intersection at all, return null.
+
+        For example, the following two linked lists begin to intersect at node c1:
+
+
+        The test cases are generated such that there are no cycles anywhere in the entire linked structure.
+
+        Note that the linked lists must retain their original structure after the function returns.
+
+        Custom Judge:
+
+        The inputs to the judge are given as follows (your program is not given these inputs):
+
+        intersectVal - The value of the node where the intersection occurs. This is 0 if there is no intersected node.
+        listA - The first linked list.
+        listB - The second linked list.
+        skipA - The number of nodes to skip ahead in listA (starting from the head) to get to the intersected node.
+        skipB - The number of nodes to skip ahead in listB (starting from the head) to get to the intersected node.
+        The judge will then create the linked structure based on these inputs and pass the two heads, headA and headB to your program. If you correctly return the intersected node, then your solution will be accepted.
+    */
+
+
+
+   public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) return null;
+
+        ListNode p1 = headA, p2 = headB;
+
+        while (p1 != p2) {
+            // If a pointer reaches the end, switch to the other list
+            p1 = (p1 == null) ? headB : p1.next;
+            p2 = (p2 == null) ? headA : p2.next;
+        }
+
+        return p1;
+    }
 
     
     
