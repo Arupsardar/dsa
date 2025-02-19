698f13485b20ae27f345edfaabcbfa5ba914fa1a
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-02-19 22:40:38 +0530

    209. Minimum Size Subarray Sum by arup

--- Solution/Solution.java
+++ Solution/Solution.java
@@ -671,6 +671,31 @@ public class Solution {
 
         return p1;
     }
+    /*
+     * 209. Minimum Size Subarray Sum
+
+        Given an array of positive integers nums and a positive integer target, return the minimal length of a 
+        subarray
+        whose sum is greater than or equal to target. If there is no such subarray, return 0 instead.
+     */
+
+    public int minSubArrayLen(int target, int[] nums) {
+        int n = nums.length;
+        int left = 0, sum = 0;
+        int minLength = Integer.MAX_VALUE;
+    
+        for (int right = 0; right < n; right++) {
+            sum += nums[right]; 
+    
+            while (sum >= target) { 
+                minLength = Math.min(minLength, right - left + 1);
+                sum -= nums[left]; 
+                left++; 
+            }
+        }
+    
+        return (minLength == Integer.MAX_VALUE) ? 0 : minLength;
+     }
 
     
     
@@ -704,6 +729,8 @@ class NumArray {
         }
         return ans;
     }
+
+    
 }
 
 class NumArray2 {
