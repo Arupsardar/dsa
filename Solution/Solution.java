ba166307f957983125c665743423ec1f2d73480e
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-03-15 16:24:23 +0530

    51. N-Queens ii  by Arup

--- Solution/Solution.java
+++ Solution/Solution.java
@@ -1507,6 +1507,35 @@ public class Solution {
         }
     }
 
+    /*
+     * 52. N-Queens II
+
+        The n-queens puzzle is the problem of placing n queens on an n x n chessboard such that no two queens attack each other.
+
+        Given an integer n, return the number of distinct solutions to the n-queens puzzle.
+     */
+
+
+    public int totalNQueens(int n) {
+        return solve(0, 0, 0, 0, n);
+    }
+    
+    private int solve(int row, int cols, int diags, int antiDiags, int n) {
+        if (row == n)
+            return 1;
+
+        int availablePositions = ((1 << n) - 1) & ~(cols | diags | antiDiags);
+
+        int solutions = 0;
+        while (availablePositions != 0) {
+            int pos = availablePositions & -availablePositions;
+            availablePositions &= availablePositions - 1;
+            solutions += solve(row + 1, cols | pos, (diags | pos) << 1, (antiDiags | pos) >> 1, n);
+        }
+
+        return solutions;
+    }
+
 
 
 
