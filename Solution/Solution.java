3af3e55223845ed0730085a71ba5da56f83800b9
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-03-18 23:09:21 +0530

    1945. Sum of Digits of String After Convert by Arup

--- Solution/Solution.java
+++ Solution/Solution.java
@@ -1682,6 +1682,82 @@ class NumArray {
         }
     }
 
+    public static int sum(int a)
+    {
+        if(a<=9)
+        {return a;}
+        int sum =0;
+        
+        while(a!=0)
+        {
+            int take= a%10;
+            sum+=take;
+            a/=10;
+        }
+     return sum;
+
+    }
+
+    /*
+     * 1945. Sum of Digits of String After Convert
+
+        You are given a string s consisting of lowercase English letters, and an integer k. Your task is to convert the string into an integer by a special process, and then transform it by summing its digits repeatedly k times. More specifically, perform the following steps:
+
+        Convert s into an integer by replacing each letter with its position in the alphabet (i.e. replace 'a' with 1, 'b' with 2, ..., 'z' with 26).
+        Transform the integer by replacing it with the sum of its digits.
+        Repeat the transform operation (step 2) k times in total.
+        For example, if s = "zbax" and k = 2, then the resulting integer would be 8 by the following operations:
+
+        Convert: "zbax" ➝ "(26)(2)(1)(24)" ➝ "262124" ➝ 262124
+        Transform #1: 262124 ➝ 2 + 6 + 2 + 1 + 2 + 4 ➝ 17
+        Transform #2: 17 ➝ 1 + 7 ➝ 8
+        Return the resulting integer after performing the operations described above.
+     */
+
+    //Bast time
+
+    public int getLucky2(String s, int k) {
+        int sum = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int it = s.charAt(i) - 'a' + 1;
+            sum += sum(it);
+
+        }
+
+        if(k>=2)
+        {
+            for(int i=2;i<=k;i++)
+            {
+                
+               sum= sum(sum);
+
+            }
+        }
+        return(sum);
+    }
+
+    //My solution
+    public int getLucky(String s, int k) {
+        String num ="";
+         for(int i=0;i<s.length();i++){
+             int num1 =s.charAt(i)-'a';
+            
+             num +=(num1+1);
+         }
+        
+         for(int i=0;i<k;i++){
+            int ans=0;
+            for(int j=0;j<num.length();j++){
+                int n =num.charAt(j)-'0';
+                ans +=n;
+            }
+            num ="";
+            num +=ans;
+
+         }
+         return  Integer.parseInt(num);
+    }
+
     
 }
 
