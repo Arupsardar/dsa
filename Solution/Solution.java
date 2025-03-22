122f16ec2af0190ef8f59afdb1a0d819a52d6697
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-03-22 21:43:19 +0530

    63. Unique Paths II by Arup

--- Solution/Solution.java
+++ Solution/Solution.java
@@ -1855,6 +1855,37 @@ class NumArray {
         return -1;
         
     }
+
+    /*
+     * 63. Unique Paths II
+
+        You are given an m x n integer array grid. There is a robot initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
+
+        An obstacle and space are marked as 1 or 0 respectively in grid. A path that the robot takes cannot include any square that is an obstacle.
+
+        Return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+
+        The testcases are generated so that the answer will be less than or equal to 2 * 109.
+     */
+
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int n =obstacleGrid.length;
+        int m =obstacleGrid[0].length;
+        int [][] arr =new int[n][m];
+        if(obstacleGrid[0][0]==1) return 0;
+        arr[0][0]=1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(obstacleGrid[i][j]==1){
+                    arr[i][j]=0;
+                }else{
+                    if(i>0) arr[i][j] +=arr[i-1][j];
+                    if(j>0) arr[i][j] +=arr[i][j-1];
+                }
+            }
+        }
+        return arr[n-1][m-1];
+    }
      
 
     
