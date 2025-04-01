4357856d0b85d4cabeb052e3ac32b7fefeb340eb
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-04-01 22:29:58 +0530

    81. Search in Rotated Sorted Array II by Arup

--- Solution/Solution.java
+++ Solution/Solution.java
@@ -2286,6 +2286,75 @@ class NumArray {
         return ans;
     }
 
+    /*
+     * Armstrong Numbers
+
+            You are given a 3-digit number n, Find whether it is an Armstrong number or not.
+
+            An Armstrong number of three digits is a number such that the sum of the cubes of its digits is equal to the number itself. 371 is an Armstrong number since 33 + 73 + 13 = 371. 
+
+
+     */
+
+    static boolean armstrongNumber(int n) {
+        long num =0;
+        int num2=n;
+        while(n>0){
+            int mod =n%10;
+            num +=(mod*mod*mod);
+            n=n/10;
+        }
+        return num ==num2;
+     }
+
+     /*
+      81. Search in Rotated Sorted Array II
+
+        There is an integer array nums sorted in non-decreasing order (not necessarily with distinct values).
+
+        Before being passed to your function, nums is rotated at an unknown pivot index k (0 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,4,4,5,6,6,7] might be rotated at pivot index 5 and become [4,5,6,6,7,0,1,2,4,4].
+
+        Given the array nums after the rotation and an integer target, return true if target is in nums, or false if it is not in nums.
+
+        You must decrease the overall operation steps as much as possible.
+      */
+
+     public boolean search(int[] nums, int target) {
+        int left =0;
+        int right =nums.length-1;
+        while(left<=right){
+            int mid =left+(right-left)/2;
+            if(nums[mid]==target){
+                return true;
+            }
+            if(nums[left]==nums[mid] && nums[right]==nums[mid]){
+                left++;
+                right--;
+                continue;
+            }
+
+            if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1; 
+                } else {
+                    left = mid + 1; 
+                }
+            } 
+            
+            else {
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1; 
+                } else {
+                    right = mid - 1; 
+                }
+            }
+
+        }
+        return false;
+    }
+        
+    
+
     
 }
 
