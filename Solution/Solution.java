d1814288f29f94c2cfb60bf15dfb9cb369c08800
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-04-04 22:21:08 +0530

    137. Single Number II

--- Solution/Solution.java
+++ Solution/Solution.java
@@ -2424,6 +2424,23 @@ class NumArray {
             
         return result;
     }
+
+    /*
+     * 137. Single Number II
+
+        Given an integer array nums where every element appears three times except for one, which appears exactly once. Find the single element and return it.
+
+        You must implement a solution with a linear runtime complexity and use only constant extra space.
+     */
+
+    public int singleNumber(int[] nums) {
+        int ones=0,twos=0;
+         for (int num: nums) {
+             ones =(ones ^ num) & ~ twos;
+             twos =(twos ^ num) & ~ones;
+         }
+         return  ones;
+     }
         
     
 
