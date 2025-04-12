fff2c260debad7d50dd63e58df9b9f8ed283640b
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-04-12 22:13:26 +0530

    724. Find Pivot Index by Arup

--- Solution/Solution.java
+++ Solution/Solution.java
@@ -2793,6 +2793,35 @@ class NumArray {
        }
        return  ans;
     }
+
+    /*
+     * 724. Find Pivot Index
+
+        Given an array of integers nums, calculate the pivot index of this array.
+
+        The pivot index is the index where the sum of all the numbers strictly to the left of the index is equal to the sum of all the numbers strictly to the index's right.
+
+        If the index is on the left edge of the array, then the left sum is 0 because there are no elements to the left. This also applies to the right edge of the array.
+
+        Return the leftmost pivot index. If no such index exists, return -1.
+     */
+
+
+    public int pivotIndex(int[] nums) {
+        int totalSum = 0;
+            for (int num : nums) {
+                totalSum += num;
+            }
+
+            int leftSum = 0;
+            for (int i = 0; i < nums.length; i++) {
+                if (leftSum == totalSum - leftSum - nums[i]) {
+                    return i;
+                }
+                leftSum += nums[i];
+            }
+            return -1;
+    }
     
 
     
