5c550ca7ecc04f32afa374575bd36d1e1db5e01e
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-04-15 22:33:09 +0530

    443. String Compression by Arup

--- Solution/Solution.java
+++ Solution/Solution.java
@@ -2894,6 +2894,49 @@ class NumArray {
 
         return  sb.length()==0 ? "/" : sb.toString();
     }
+
+    /*
+     * 443. String Compression
+
+        Given an array of characters chars, compress it using the following algorithm:
+
+        Begin with an empty string s. For each group of consecutive repeating characters in chars:
+
+        If the group's length is 1, append the character to s.
+        Otherwise, append the character followed by the group's length.
+        The compressed string s should not be returned separately, but instead, be stored in the input character array chars. Note that group lengths that are 10 or longer will be split into multiple characters in chars.
+
+        After you are done modifying the input array, return the new length of the array.
+
+        You must write an algorithm that uses only constant extra space.
+     */
+
+    public int compress(char[] chars) {
+        int n = chars.length;
+      int write = 0;
+      int read = 0;
+  
+      while (read < n) {
+          int start = read;
+          while (read < n && chars[read] == chars[start]) {
+              read++;
+          }
+  
+          // Write the character
+          chars[write++] = chars[start];
+  
+          // Write the count if more than 1
+          int count = read - start;
+          if (count > 1) {
+              for (char c : String.valueOf(count).toCharArray()) {
+                  chars[write++] = c;
+              }
+          }
+      }
+  
+      return write;
+  
+      }
     
 
     
