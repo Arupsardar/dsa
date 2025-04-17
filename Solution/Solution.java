97129d902ea897c69bc1df957b8951a9ad4a132d
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-04-17 22:56:42 +0530

    342. Power of Four by Arup

--- Solution/Solution.java
+++ Solution/Solution.java
@@ -2972,6 +2972,18 @@ class NumArray {
         return output;
     }
 
+    /*
+     * 342. Power of Four
+
+        Given an integer n, return true if it is a power of four. Otherwise, return false.
+
+        An integer n is a power of four, if there exists an integer x such that n == 4x.
+     */
+
+    public boolean isPowerOfFour(int n) {
+        return n > 0 && (n & (n - 1)) == 0 && (n % 3 == 1);
+    }
+
     
 
     
