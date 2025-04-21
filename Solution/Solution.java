a4df3d3fc451e1fa57ff026dbc12e917f11fc500
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-04-21 22:14:26 +0530

    59. Spiral Matrix II Arup

--- Solution/Solution.java
+++ Solution/Solution.java
@@ -3131,6 +3131,53 @@ class NumArray {
         return score;
     }
 
+    /*
+     * 59. Spiral Matrix II
+
+        Given a positive integer n, generate an n x n matrix filled with elements from 1 to n2 in spiral order.
+     */
+
+
+    public int[][] generateMatrix(int n) {
+        int[][] ans = new int[n][n];
+
+        int top = 0, bottom = n - 1;
+        int left = 0, right = n - 1;
+        int num = 1;
+
+        while (top <= bottom && left <= right) {
+            // Left to right
+            for (int i = left; i <= right; i++) {
+                ans[top][i] = num++;
+            }
+            top++;
+
+            // Top to bottom
+            for (int i = top; i <= bottom; i++) {
+                ans[i][right] = num++;
+            }
+            right--;
+
+            // Right to left
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    ans[bottom][i] = num++;
+                }
+                bottom--;
+            }
+
+            // Bottom to top
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    ans[i][left] = num++;
+                }
+                left++;
+            }
+        }
+
+        return ans;
+    }
+
     
 
     
