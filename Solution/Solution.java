29c0c6baf0bbf2c012d912d02378826c22b2e39c
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-04-25 22:15:31 +0530

    89. Gray Code

--- Solution/Solution.java
+++ Solution/Solution.java
@@ -3308,6 +3308,35 @@ class NumArray {
         return ans;
     }
 
+    /*
+     * 89. Gray Code
+
+        An n-bit gray code sequence is a sequence of 2n integers where:
+
+        Every integer is in the inclusive range [0, 2n - 1],
+        The first integer is 0,
+        An integer appears no more than once in the sequence,
+        The binary representation of every pair of adjacent integers differs by exactly one bit, and
+        The binary representation of the first and last integers differs by exactly one bit.
+        Given an integer n, return any valid n-bit gray code sequence.
+     */
+
+
+    public List<Integer> grayCode(int n) {
+        List<Integer> list =new ArrayList<>();
+        list.add(0);
+        if(n==0) return list;
+        list.add(1);
+        int cur =1;
+        for(int i=2;i<=n;i++){
+           cur *=2;
+           for(int j=list.size()-1;j>=0;j--){
+               list.add(cur+list.get(j));
+           }
+        }
+        return list; 
+    }
+
     
 
     
