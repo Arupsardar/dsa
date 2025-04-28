0416d7568aa5c7aba219e190325e66fac04e4a66
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-04-28 22:31:17 +0530

    717. 1-bit and 2-bit Characters by arup

--- Solution/Solution.java
+++ Solution/Solution.java
@@ -3498,6 +3498,25 @@ class NumArray {
         return count;
     }
 
+    /*
+     * 717. 1-bit and 2-bit Characters
+
+        We have two special characters:
+
+        The first character can be represented by one bit 0.
+        The second character can be represented by two bits (10 or 11).
+        Given a binary array bits that ends with 0, return true if the last character must be a one-bit character.
+     */
+
+
+    public boolean isOneBitCharacter(int[] bits) {
+        int i=0;
+        while (i<bits.length-1){
+            i =(bits[i]==1)?i+2:i+1;
+        }
+        return  i==bits.length-1; 
+    }
+
     
 
     
