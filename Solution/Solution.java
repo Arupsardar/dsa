87a45d19f11b62e17cfe0cd17275d122a8802186
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-05-03 20:54:55 +0530

    142. Linked List Cycle II

--- Solution/Solution.java
+++ Solution/Solution.java
@@ -3618,6 +3618,37 @@ class NumArray {
         
     }
 
+    /*
+     * 142. Linked List Cycle II
+
+        Given the head of a linked list, return the node where the cycle begins. If there is no cycle, return null.
+
+        There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to (0-indexed). It is -1 if there is no cycle. Note that pos is not passed as a parameter.
+
+        Do not modify the linked list.
+     */
+
+
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) return null;
+        ListNode slow =head;
+        ListNode fast =head;
+        while(fast != null && fast.next != null){
+            slow =slow.next;
+            fast =fast.next.next;
+            if(slow ==fast){
+                break;
+            }
+        }
+        if (fast == null || fast.next == null) return null;
+        slow =head;
+        while(slow !=fast){
+            slow =slow.next;
+            fast =fast.next;
+        }
+        return slow;
+    }
+
     
 
     
