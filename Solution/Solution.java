6d1f842eed7c87d21b1d0b3621134bc0f81841d3
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-05-13 22:28:00 +0530

    216. Combination Sum III

--- Solution/Solution.java
+++ Solution/Solution.java
@@ -3986,6 +3986,39 @@ class NumArray {
         return deletions;
     }
 
+    /*
+     * 216. Combination Sum III
+
+        Find all valid combinations of k numbers that sum up to n such that the following conditions are true:
+
+        Only numbers 1 through 9 are used.
+        Each number is used at most once.
+        Return a list of all possible valid combinations. The list must not contain the same combination twice, and the combinations may be returned in any order.
+     */
+
+
+     public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(1, k, n, new ArrayList<>(), result);
+        return result;
+    }
+
+    private void backtrack(int start, int k, int n, List<Integer> path, List<List<Integer>> result) {
+        if (path.size() == k) {
+            if (n == 0) {
+                result.add(new ArrayList<>(path));
+            }
+            return;
+        }
+
+        for (int i = start; i <= 9; i++) {
+            if (i > n) break;
+            path.add(i);
+            backtrack(i + 1, k, n - i, path, result);
+            path.remove(path.size() - 1);
+        }
+    }
+
 
 
 
