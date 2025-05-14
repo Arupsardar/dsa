395c9b108711bc951fd178dbf1d370a7a4b843d3
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-05-14 22:07:37 +0530

    216.234. Palindrome Linked List

--- Solution/Solution.java
+++ Solution/Solution.java
@@ -4048,6 +4048,49 @@ class NumArray {
         return res;
     }
 
+
+    /*
+     * 234. Palindrome Linked List
+
+        Given the head of a singly linked list, return true if it is a palindrome or false otherwise.
+     */
+
+     public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) return true;
+
+        // Step 1: Find middle of the list
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // Step 2: Reverse second half
+        ListNode secondHalf = reverse(slow);
+
+        // Step 3: Compare both halves
+        ListNode firstHalf = head;
+        while (secondHalf != null) {
+            if (firstHalf.val != secondHalf.val)
+                return false;
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
+        }
+
+        return true;
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode nextNode = head.next;
+            head.next = prev;
+            prev = head;
+            head = nextNode;
+        }
+        return prev;
+    }
+
     
 
     
