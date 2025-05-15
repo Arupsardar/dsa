18ffe745a593792cb0651ae23ea12ce27cf4735e
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-05-15 21:58:12 +0530

    2900. Longest Unequal Adjacent Groups Subsequence I

--- Solution/Solution.java
+++ Solution/Solution.java
@@ -4091,6 +4091,38 @@ class NumArray {
         return prev;
     }
 
+    /*
+     * 2900. Longest Unequal Adjacent Groups Subsequence I
+
+        You are given a string array words and a binary array groups both of length n, where words[i] is associated with groups[i].
+
+        Your task is to select the longest alternating subsequence from words. A subsequence of words is alternating if for any two consecutive strings in the sequence, their corresponding elements in the binary array groups differ. Essentially, you are to choose strings such that adjacent elements have non-matching corresponding bits in the groups array.
+
+        Formally, you need to find the longest subsequence of an array of indices [0, 1, ..., n - 1] denoted as [i0, i1, ..., ik-1], such that groups[ij] != groups[ij+1] for each 0 <= j < k - 1 and then find the words corresponding to these indices.
+
+        Return the selected subsequence. If there are multiple answers, return any of them.
+
+        Note: The elements in words are distinct.
+     */
+
+
+    public List<String> getLongestSubsequence(String[] words, int[] groups) {
+        List<String> result = new ArrayList<>();
+        if (words.length == 0) return result;
+
+        result.add(words[0]);
+        int prevGroup = groups[0];
+
+        for (int i = 1; i < words.length; i++) {
+            if (groups[i] != prevGroup) {
+                result.add(words[i]);
+                prevGroup = groups[i];
+            }
+        }
+
+        return result;
+    }
+
     
 
     
