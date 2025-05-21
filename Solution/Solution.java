e09139e6c2c94f8fb16440afa30fcab38bac6461
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-05-21 22:09:15 +0530

    287. Find the Duplicate Number1

--- Solution/Solution.java
+++ Solution/Solution.java
@@ -4223,6 +4223,23 @@ class NumArray {
         return sb.toString();
     }
 
+    /*
+     * 709. To Lower Case
+
+        Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+     */
+
+
+    public String toLowerCase(String s) {
+        char [] arr =s.toCharArray();
+        for(int i =0;i<arr.length;i++){
+            if (arr[i] >= 'A' && arr[i] <= 'Z') {
+                arr[i] = (char)(arr[i] + 32); // Convert to lowercase
+            }
+        }
+        return  new String(arr);
+    }
+
     
 
     
