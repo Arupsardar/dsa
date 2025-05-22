d6861ed1099589a3c3afcc3f1206abad241e2930
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-05-22 22:13:58 +0530

    287. Find the Duplicate Number2

--- Solution/Solution.java
+++ Solution/Solution.java
@@ -4240,6 +4240,37 @@ class NumArray {
         return  new String(arr);
     }
 
+    /*
+     * 2129. Capitalize the Title
+
+        You are given a string title consisting of one or more words separated by a single space, where each word consists of English letters. Capitalize the string by changing the capitalization of each word such that:
+
+        If the length of the word is 1 or 2 letters, change all letters to lowercase.
+        Otherwise, change the first letter to uppercase and the remaining letters to lowercase.
+        Return the capitalized title.
+     */
+
+
+    public String capitalizeTitle(String title) {
+       String[] words = title.toLowerCase().split(" ");
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+            if (word.length() > 2) {
+                sb.append(Character.toUpperCase(word.charAt(0)));
+                sb.append(word.substring(1));
+            } else {
+                sb.append(word); // keep short words lowercase
+            }
+            if (i != words.length - 1) {
+                sb.append(" ");
+            }
+        }
+
+        return sb.toString();
+    }
+
     
 
     
