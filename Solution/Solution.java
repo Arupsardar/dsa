108a78a4c428ea2e60bfc954720c73455102036f
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-05-29 23:25:31 +0530

    190. Reverse Bits

--- Solution/Solution.java
+++ Solution/Solution.java
@@ -4496,6 +4496,23 @@ class NumArray {
       
               return length;  
           }
+          /*
+           * 190. Reverse Bits
+
+
+                Note that in some languages, such as Java, there is no unsigned integer type. In this case, both input and output will be given as a signed integer type. They should not affect your implementation, as the integer's internal binary representation is the same, whether it is signed or unsigned.
+                In Java, the compiler represents the signed integers using 2's complement notation. Therefore, in Example 2 above, the input represents the signed integer -3 and the output represents the signed integer -1073741825.
+           */
+
+        public int reverseBits(int n) {
+            int ans = 0;
+            for (int i = 0; i < 32; i++) {
+                ans <<= 1;         // Shift result left by 1 bit
+                ans |= (n & 1);    // Add the last bit of n to ans
+                n >>= 1;           // Shift n right by 1 bit
+            }
+            return ans;
+        }
 
     
 
