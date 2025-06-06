83dc800fcb308c5eb2abf0310a409de6561ae1c4
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-06-06 22:33:23 +0530

    2413. Smallest Even Multiple

--- Solution/Solution.java
+++ Solution/Solution.java
@@ -4754,6 +4754,22 @@ class NumArray {
 
         }
 
+        /*
+         * 2413. Smallest Even Multiple
+
+            Given a positive integer n, return the smallest positive integer that is a multiple of both 2 and n.
+         */
+
+
+        public int smallestEvenMultiple(int n) {
+            if(n%2==0){
+                return n;
+            }else{
+                return n*2;
+            }
+            
+        }
+
     
 
     
