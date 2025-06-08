0a9307f8264046eda07816bbf664143bf9c75bff
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-06-08 23:22:32 +0530

    2384. Largest Palindromic Number

--- Solution/Solution.java
+++ Solution/Solution.java
@@ -4891,6 +4891,40 @@ class NumArray {
         return firstHalf.toString() + middle + secondHalf;
     }
 
+    /*
+     * 2341. Maximum Number of Pairs in Array
+
+            You are given a 0-indexed integer array nums. In one operation, you may do the following:
+
+            Choose two integers in nums that are equal.
+            Remove both integers from nums, forming a pair.
+            The operation is done on nums as many times as possible.
+
+            Return a 0-indexed integer array answer of size 2 where answer[0] is the number of pairs that are formed and answer[1] is the number of leftover integers in nums after doing the operation as many times as possible.
+     */
+
+    public int[] numberOfPairs(int[] nums) {
+        int [] nu =new int[101];
+        for(int i=0;i<nums.length;i++){
+            nu[nums[i]]++;
+        }
+        int paircount =0;
+        int oddcount=0;
+        for(int i=0;i<101;i++){
+            if(nu[i]>0){
+                int mod =nu[i]%2;
+                int div =nu[i]/2;
+                paircount +=div;
+                oddcount +=mod;
+            }
+            
+        }
+
+        int[] arr ={paircount,oddcount};
+
+        return arr;
+    }
+
     
 
     
