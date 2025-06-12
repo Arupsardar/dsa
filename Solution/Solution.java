4ded51321c092e4e32522a012df4b6746f0ce172
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-06-12 22:44:28 +0530

    461. Hamming Distance

--- Solution/Solution.java
+++ Solution/Solution.java
@@ -4996,6 +4996,26 @@ class NumArray {
         return total;
     }
 
+    /*
+     * 461. Hamming Distance
+
+            The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
+
+            Given two integers x and y, return the Hamming distance between them.
+        */
+
+
+    public int hammingDistance(int x, int y) {
+        int xor =x^y;
+        int asn =0;
+        for(int i=0;i<32;i++){
+            if(((xor>>i)&1)==1){
+                asn++;
+            }
+        }
+        return  asn;
+    }
+
     
 
     
