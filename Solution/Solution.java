64916a1d56cb1259c414b98c29d4f3631a494935
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-06-13 22:43:46 +0530

    3133. Minimum Array End

--- Solution/Solution.java
+++ Solution/Solution.java
@@ -5016,6 +5016,41 @@ class NumArray {
         return  asn;
     }
 
+    /*
+     * 3133. Minimum Array End
+
+        You are given two integers n and x. You have to construct an array of positive integers nums of size n where for every 0 <= i < n - 1, nums[i + 1] is greater than nums[i], and the result of the bitwise AND operation between all elements of nums is x.
+
+        Return the minimum possible value of nums[n - 1].
+     */
+
+
+    public long minEnd2(int n, int x) {
+        long a =x;
+        while(--n>0){
+            a =(a+1)|x;
+        }
+        return a;
+    }
+
+
+    public long minEnd(int n, int x) {
+        long result = x;
+        n--;
+
+        for (int i = 0; n > 0 ; i++) {
+            if ((x & 1) == 0) {
+                if((n & 1) == 1){
+                    result |= (1L << i);
+                }
+                n >>= 1;
+            }
+            x >>= 1;
+        }
+
+        return result;
+    }
+
     
 
     
