e1b931c460d8cd35807db54f3814b16320216b2f
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-06-17 22:49:48 +0530

    868. Binary Gap

--- Solution/Solution.java
+++ Solution/Solution.java
@@ -5152,6 +5152,31 @@ class NumArray {
         return ans;
     }
 
+    /*
+     * Given a positive integer n, find and return the longest distance between any two adjacent 1's in the binary representation of n. If there are no two adjacent 1's, return 0.
+
+        Two 1's are adjacent if there are only 0's separating them (possibly no 0's). The distance between two 1's is the absolute difference between their bit positions. For example, the two 1's in "1001" have a distance of 3.
+     */
+
+    public int binaryGap(int n) {
+        int last = -1;
+        int maxGap = 0;
+        int pos = 0;
+
+        while (n > 0) {
+            if ((n & 1) == 1) {
+                if (last != -1) {
+                    maxGap = Math.max(maxGap, pos - last);
+                }
+                last = pos;
+            }
+            n >>= 1;
+            pos++;
+        }
+
+        return maxGap;
+    }
+
     
 
     
