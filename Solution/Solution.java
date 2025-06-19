75f7d79d911f1584025c13715d66705e397587fc
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-06-19 22:56:04 +0530

    1356. Sort Integers by The Number of 1 Bits

--- Solution/Solution.java
+++ Solution/Solution.java
@@ -5196,6 +5196,25 @@ class NumArray {
         return ans;
     }
 
+    /*
+     * 1356. Sort Integers by The Number of 1 Bits
+
+        You are given an integer array arr. Sort the integers in the array in ascending order by the number of 1's in their binary representation and in case of two or more integers have the same number of 1's you have to sort them in ascending order.
+
+        Return the array after sorting it.
+     */
+
+
+    public int[] sortByBits(int[] arr) {
+        for(int i=0;i<arr.length;i++){
+            arr[i]+=Integer.bitCount(arr[i])*10001;
+
+        }Arrays.sort(arr);
+        for(int i=0;i<arr.length;i++){
+            arr[i]=arr[i]%10001;
+        }return arr;
+    }
+
     
 
     
