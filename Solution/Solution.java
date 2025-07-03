b7d7319afa917839a3a6b31a1ad703c1cd84c6e1
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-07-03 22:43:32 +0530

    696. Count Binary Substrings d

--- Solution/Solution.java
+++ Solution/Solution.java
@@ -5523,6 +5523,33 @@ class NumArray {
         return count;
     }
 
+    /*
+     * 696. Count Binary Substrings
+
+        Given a binary string s, return the number of non-empty substrings that have the same number of 0's and 1's, and all the 0's and all the 1's in these substrings are grouped consecutively.
+
+        Substrings that occur multiple times are counted the number of times they occur.
+     */
+
+    public int countBinarySubstrings(String s) {
+        int prevRunLength = 0;
+        int currRunLength = 1;
+        int count = 0;
+
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == s.charAt(i - 1)) {
+                currRunLength++;
+            } else {
+                count += Math.min(prevRunLength, currRunLength);
+                prevRunLength = currRunLength;
+                currRunLength = 1;
+            }
+        }
+
+        count += Math.min(prevRunLength, currRunLength); // for the last group
+        return count;
+    }
+
 
     
 
