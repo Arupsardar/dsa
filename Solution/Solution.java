2959157274bebeef64e50d9330f6a2340bc51e09
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-07-06 21:09:02 +0530

    696. Count Binary Substrings e2

--- Solution/Solution.java
+++ Solution/Solution.java
@@ -5641,6 +5641,24 @@ class NumArray {
     }
 
 
+    public int firstUniqChar(String s) {
+        int[] freq = new int[26];  // For lowercase a-z
+
+        // First pass: Count frequency of each character
+        for (char c : s.toCharArray()) {
+            freq[c - 'a']++;
+        }
+
+        // Second pass: Find first character with frequency 1
+        for (int i = 0; i < s.length(); i++) {
+            if (freq[s.charAt(i) - 'a'] == 1) {
+                return i;
+            }
+        }
+
+        return -1; 
+    }
+
     
 
     
