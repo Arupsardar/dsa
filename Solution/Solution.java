a84e04bd6837f1a6ac880b5b425a793969d5f487
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-07-09 22:48:30 +0530

    349. Intersection of Two Arrays

--- Solution/Solution.java
+++ Solution/Solution.java
@@ -5680,6 +5680,38 @@ class NumArray2 {
     public int sumRange(int left, int right) {
       return nums2[right+1]- nums2[left];
     }
+
+    /*
+     * 349. Intersection of Two Arrays
+
+        Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.
+     */
+
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> resultSet = new HashSet<>();
+
+        // Add all elements of nums1 to set1
+        for (int num : nums1) {
+            set1.add(num);
+        }
+
+        // Check which elements of nums2 are in set1
+        for (int num : nums2) {
+            if (set1.contains(num)) {
+                resultSet.add(num);
+            }
+        }
+
+        // Convert resultSet to int[]
+        int[] result = new int[resultSet.size()];
+        int i = 0;
+        for (int num : resultSet) {
+            result[i++] = num;
+        }
+
+        return result;
+    }
 }
 
 /*
