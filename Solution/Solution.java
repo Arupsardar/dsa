7c5240b44149defa8e7b75f0c47cfaa40292eeff
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-07-11 22:14:00 +0530

    349. Intersection of Two Array2

--- Solution/Solution.java
+++ Solution/Solution.java
@@ -5801,6 +5801,23 @@ class MyStack {
         answer.add(diff2);
         return answer; 
     }
+
+
+    public List<Integer> intersection(int[][] nums) {
+        List<Integer> ans = new ArrayList<>();
+        int[] count  = new int[1001];
+        for(int[] arr : nums){
+            for(int i : arr){
+                count[i]++;
+            }
+        }
+       for(int i=0;i<count.length;i++){
+           if(count[i]==nums.length){
+               ans.add(i);
+           }
+       }
+        return ans;
+    }
 }
 
 
