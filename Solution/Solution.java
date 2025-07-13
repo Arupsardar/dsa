40d7be23e6fb7b54179756eeda68f852894a43ef
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-07-13 21:09:44 +0530

    349. Intersection of Two Array2

--- Solution/Solution.java
+++ Solution/Solution.java
@@ -5845,6 +5845,36 @@ class MyStack {
 
         return output;
     }
+
+    public List<TreeNode> generateTrees(int n) {
+        if (n == 0) return new ArrayList<>();
+        return buildTrees(1, n);
+    }
+
+    private List<TreeNode> buildTrees(int start, int end) {
+        List<TreeNode> result = new ArrayList<>();
+        
+        if (start > end) {
+            result.add(null); // Empty tree
+            return result;
+        }
+
+        for (int i = start; i <= end; i++) {
+            List<TreeNode> leftSubtrees = buildTrees(start, i - 1);
+            List<TreeNode> rightSubtrees = buildTrees(i + 1, end);
+            
+            for (TreeNode left : leftSubtrees) {
+                for (TreeNode right : rightSubtrees) {
+                    TreeNode root = new TreeNode(i);
+                    root.left = left;
+                    root.right = right;
+                    result.add(root);
+                }
+            }
+        }
+        
+        return result;
+    }
 }
 
 
