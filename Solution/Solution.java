7fd6aa1b4c292ccd6aff86a5eca45802374fcf27
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-07-14 22:28:45 +0530

    349. Intersection of Two

--- Solution/Solution.java
+++ Solution/Solution.java
@@ -5875,6 +5875,23 @@ class MyStack {
         
         return result;
     }
+
+    public int numTrees(int n) {
+        int[] dp = new int[n + 1];
+         
+         dp[0] = 1; // Empty tree
+         dp[1] = 1; // One node tree
+ 
+         for (int nodes = 2; nodes <= n; nodes++) {
+             for (int root = 1; root <= nodes; root++) {
+                 int left = root - 1;
+                 int right = nodes - root;
+                 dp[nodes] += dp[left] * dp[right];
+             }
+         }
+ 
+         return dp[n];
+     }
 }
 
 
