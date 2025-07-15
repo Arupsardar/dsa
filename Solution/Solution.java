6bc4f9fb65ad3e30e15aca1dd6ca69a2a7da1df5
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-07-15 22:17:33 +0530

    349. Intersection of Twp

--- Solution/Solution.java
+++ Solution/Solution.java
@@ -5892,6 +5892,26 @@ class MyStack {
  
          return dp[n];
      }
+
+    int index = 0;
+    public TreeNode bstFromPreorder(int[] preorder) {
+        return build(preorder, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+
+    private TreeNode build(int[] preorder, int min, int max) {
+        if (index >= preorder.length) return null;
+
+        int val = preorder[index];
+        if (val < min || val > max) return null;
+
+        TreeNode root = new TreeNode(val);
+        index++;
+
+        root.left = build(preorder, min, val - 1);
+        root.right = build(preorder, val + 1, max);
+
+        return root;
+    }
 }
 
 
