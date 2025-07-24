a29c7cc7207b719ef899e556551006428e3fb7a4
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-07-24 21:15:15 +0530

    349. Intersection of Tw3

--- Solution/Solution.java
+++ Solution/Solution.java
@@ -6000,6 +6000,23 @@ class MyStack {
 
     return n - 2 * pairs;
     }
+
+    public char slowestKey(int[] releaseTimes, String keysPressed) {
+        int maxDuration = releaseTimes[0];
+        char result = keysPressed.charAt(0);
+    
+        for (int i = 1; i < releaseTimes.length; i++) {
+            int duration = releaseTimes[i] - releaseTimes[i - 1];
+            char key = keysPressed.charAt(i);
+    
+            if (duration > maxDuration || (duration == maxDuration && key > result)) {
+                maxDuration = duration;
+                result = key;
+            }
+        }
+    
+        return result; 
+     }
 }
 
 
