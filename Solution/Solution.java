9aa121c703d7bd36b9ea2167aa3b7d28ccbc268b
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-07-26 22:26:52 +0530

    349. Intersection of Twsde

--- Solution/Solution.java
+++ Solution/Solution.java
@@ -6041,6 +6041,16 @@ class MyStack {
 
         return i + j == words1.length;
     }
+
+    public int[] buildArray(int[] nums) {
+        int n =nums.length;
+        int [] ans =new int[n];
+        for(int i=0;i<n;i++){
+            ans[i]=nums[nums[i]];
+        }
+        return ans;
+        
+    }
 }
 
 
