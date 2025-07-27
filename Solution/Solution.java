7eea1b5e5e2c17021bf769938682d66bfdcfaffc
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-07-27 21:36:35 +0530

    349. Intersection of Twsde

--- Solution/Solution.java
+++ Solution/Solution.java
@@ -6051,6 +6051,34 @@ class MyStack {
         return ans;
         
     }
+
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+      
+        for (String token : tokens) {
+            if (token.equals("+")) {
+                int b = stack.pop();
+                int a = stack.pop();
+                stack.push(a + b);
+            } else if (token.equals("-")) {
+                int b = stack.pop();
+                int a = stack.pop();
+                stack.push(a - b);
+            } else if (token.equals("*")) {
+                int b = stack.pop();
+                int a = stack.pop();
+                stack.push(a * b);
+            } else if (token.equals("/")) {
+                int b = stack.pop();
+                int a = stack.pop();
+                stack.push(a / b);
+            } else {
+                stack.push(Integer.parseInt(token));
+            }
+        }
+    
+        return stack.pop(); 
+      }
 }
 
 
