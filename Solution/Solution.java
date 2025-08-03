cfada023da50558f59fd6a5a4e5f30c607068080
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-08-03 20:19:02 +0530

    97. Interleaving Strinl

--- Solution/Solution.java
+++ Solution/Solution.java
@@ -6264,6 +6264,32 @@ class MyStack {
         return dp[m][n];
     }
 
+    public int longestConsecutive(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+
+        int longest = 0;
+
+        for (int num : set) {
+            // Check only if it's the start of a sequence
+            if (!set.contains(num - 1)) {
+                int currentNum = num;
+                int streak = 1;
+
+                while (set.contains(currentNum + 1)) {
+                    currentNum++;
+                    streak++;
+                }
+
+                longest = Math.max(longest, streak);
+            }
+        }
+
+        return longest;
+    }
+
     
 }
 
