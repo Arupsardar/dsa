07bba1e8e1b6057cec4f3311771f6ebadfc5fbfe
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-08-05 20:42:52 +0530

    add new branch

--- Solution/Solution.java
+++ Solution/Solution.java
@@ -6325,6 +6325,27 @@ class MyStack {
         return max;
     }
 
+
+    public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
+        int n = fruits.length;
+        boolean[] used = new boolean[baskets.length];
+        int unplaced = 0;
+
+        for (int i = 0; i < n; i++) {
+            boolean placed = false;
+            for (int j = 0; j < baskets.length; j++) {
+                if (!used[j] && baskets[j] >= fruits[i]) {
+                    used[j] = true;  // Mark basket as used
+                    placed = true;
+                    break;
+                }
+            }
+            if (!placed) unplaced++;
+        }
+
+        return unplaced;
+    }
+
     
 }
 
