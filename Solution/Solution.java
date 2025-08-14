1b499d883ef3db3c252f531aa680be52811b8599
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-08-14 22:07:32 +0530

    add new brancsddfddffs

--- Solution/Solution.java
+++ Solution/Solution.java
@@ -6572,6 +6572,21 @@ class MyStack {
         return n == 1;
     }
 
+
+    public String largestGoodInteger(String num) {
+        String maxGood = "";
+        
+        for (int i = 0; i <= num.length() - 3; i++) {
+            String sub = num.substring(i, i + 3);
+            if (sub.charAt(0) == sub.charAt(1) && sub.charAt(1) == sub.charAt(2)) {
+                if (maxGood.equals("") || sub.compareTo(maxGood) > 0) {
+                    maxGood = sub;
+                }
+            }
+        }
+        return maxGood;
+    }
+
     
 }
 
