0f44fbf7e9ecfd3977a38a205981032402a725a5
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-08-16 21:45:20 +0530

    add new brancsddfddff4

--- Solution/Solution.java
+++ Solution/Solution.java
@@ -6596,6 +6596,20 @@ class MyStack {
         return maxGood;
     }
 
+
+    public int maximum69Number (int num) {
+         char[] digits = String.valueOf(num).toCharArray();
+    
+        for (int i = 0; i < digits.length; i++) {
+            if (digits[i] == '6') {
+                digits[i] = '9';   // Change the first 6 to 9
+                break;             // Only one change allowed
+            }
+        }
+        
+        return Integer.parseInt(new String(digits));
+    }
+
     
 }
 
