ace3731fbc619cc88bb377798070bcb20a7764ef
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-08-21 22:22:20 +0530

    add new brancsddf

--- Solution/Solution.java
+++ Solution/Solution.java
@@ -6724,6 +6724,33 @@ class MyStack {
         return ans;
     }
 
+
+    public int numSubmat(int[][] mat) {
+        int m = mat.length, n = mat[0].length;
+        int[][] heights = new int[m][n];
+        
+        // Build histogram heights
+        for (int j = 0; j < n; j++) {
+            for (int i = 0; i < m; i++) {
+                if (mat[i][j] == 0) heights[i][j] = 0;
+                else heights[i][j] = (i == 0 ? 1 : heights[i-1][j] + 1);
+            }
+        }
+        
+        int ans = 0;
+        // For each row, count submatrices ending at that row
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int minHeight = heights[i][j];
+                for (int k = j; k >= 0 && minHeight > 0; k--) {
+                    minHeight = Math.min(minHeight, heights[i][k]);
+                    ans += minHeight;
+                }
+            }
+        }
+        return ans;
+    }
+
     
 }
 
