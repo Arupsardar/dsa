c1ebb195b57c01b42c4605b7c13073c303e45214
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-08-26 22:00:49 +0530

    add new brancsddg

--- Solution/Solution.java
+++ Solution/Solution.java
@@ -6831,6 +6831,27 @@ class MyStack {
         return maxLen - 1;
     }
 
+    public int areaOfMaxDiagonal(int[][] dimensions) {
+        double maxDiagonal = 0;
+        int maxArea = 0;
+
+        for (int[] rect : dimensions) {
+            int length = rect[0];
+            int width = rect[1];
+
+            double diagonal = Math.sqrt((long)length * length + (long)width * width);
+            int area = length * width;
+
+            if (diagonal > maxDiagonal) {
+                maxDiagonal = diagonal;
+                maxArea = area;
+            } else if (diagonal == maxDiagonal && area > maxArea) {
+                maxArea = area;
+            }
+        }
+        return maxArea;
+    }
+
     
 }
 
