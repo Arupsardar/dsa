ca8792d95ee034f7c985648700764b6303ac378a
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-08-31 21:54:26 +0530

    add new brancsddgfjffgd

--- Solution/Solution.java
+++ Solution/Solution.java
@@ -6978,6 +6978,30 @@ class MyStack {
         }
     }
 
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) return result;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            TreeNode node = null;
+
+            for (int i = 0; i < size; i++) {
+                node = queue.poll();
+                if (node.left != null) queue.offer(node.left);
+                if (node.right != null) queue.offer(node.right);
+            }
+
+            // last node of this level
+            result.add(node.val);
+        }
+
+        return result;
+    }
+
     
 }
 
