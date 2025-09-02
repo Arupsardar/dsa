e895a3bbe0e4d7c4ac799392a84d30969c124a4a
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-09-02 22:33:02 +0530

    add new brancsddgfjffgdsdd

--- Solution/Solution.java
+++ Solution/Solution.java
@@ -7035,6 +7035,42 @@ class MyStack {
         double gain() {
             return (double)(pass + 1) / (total + 1) - (double)pass / total;
         }
+    } public List<Integer> largestDivisibleSubset(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+
+        // dp[i] = size of the largest subset ending at nums[i]
+        int[] dp = new int[n];
+        int[] parent = new int[n];
+        Arrays.fill(dp, 1);
+        Arrays.fill(parent, -1);
+
+        int maxLen = 1, maxIndex = 0;
+
+        // Build DP
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] % nums[j] == 0) {
+                    if (dp[j] + 1 > dp[i]) {
+                        dp[i] = dp[j] + 1;
+                        parent[i] = j;
+                    }
+                }
+            }
+            if (dp[i] > maxLen) {
+                maxLen = dp[i];
+                maxIndex = i;
+            }
+        }
+
+        // Reconstruct subset
+        List<Integer> result = new ArrayList<>();
+        while (maxIndex != -1) {
+            result.add(nums[maxIndex]);
+            maxIndex = parent[maxIndex];
+        }
+        Collections.reverse(result);
+        return result;
     }
 
     
