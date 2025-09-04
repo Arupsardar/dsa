5e93ba6c6c83ecbcf9fb0726952883a52d4f14da
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-09-04 22:55:53 +0530

    add new brancsddgfjf

--- Solution/Solution.java
+++ Solution/Solution.java
@@ -7098,6 +7098,18 @@ class MyStack {
         return count;
     }
 
+    public int findClosest(int x, int y, int z) {
+        int d1 =Math.abs(z-x);
+        int d2 =Math.abs(z-y);
+        if(d1==d2){
+            return 0;
+        }else if(d1>d2){
+            return 2;
+        }else{
+            return 1;
+        }
+    }
+
     
 }
 
