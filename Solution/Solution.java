f2e1965e55349d351141b153bc2d8f01f0e7f5b7
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-09-08 22:02:35 +0530

    add new brancsddgfjdddd

--- Solution/Solution.java
+++ Solution/Solution.java
@@ -7167,6 +7167,25 @@ class MyStack {
         return res;
     }
 
+     private static boolean noZero(int x) {
+        while (x > 0) {
+            if (x % 10 == 0) return false;
+            x /= 10;
+        }
+        return true;
+    }
+    
+    public int[] getNoZeroIntegers(int n) {
+        for (int a = 1; a < n; a++) {
+            int b = n - a;
+            if (noZero(a) && noZero(b)) {
+                return new int[]{a, b};
+            }
+        }
+        // Problem statement guarantees a solution exists, but return fallback
+        return new int[]{-1, -1};
+    }
+
     
 }
 
