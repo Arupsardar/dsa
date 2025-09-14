472c9b5349af43e7df28146fd4676e806b0b0f1b
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-09-14 23:23:26 +0530

    add new brancsddgfjddddhg

--- Solution/Solution.java
+++ Solution/Solution.java
@@ -7330,6 +7330,52 @@ class MyStack {
         return maxVowel + maxConsonant;
     }
 
+    public String[] spellchecker(String[] wordlist, String[] queries) {
+        Set<String> exact = new HashSet<>();
+        Map<String, String> caseMap = new HashMap<>();
+        Map<String, String> vowelMap = new HashMap<>();
+        
+        for (String word : wordlist) {
+            exact.add(word);
+            
+            String lower = word.toLowerCase();
+            caseMap.putIfAbsent(lower, word);
+            
+            String devowel = devowel(lower);
+            vowelMap.putIfAbsent(devowel, word);
+        }
+        
+        String[] ans = new String[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            String q = queries[i];
+            if (exact.contains(q)) {
+                ans[i] = q;
+                continue;
+            }
+            String lowerQ = q.toLowerCase();
+            if (caseMap.containsKey(lowerQ)) {
+                ans[i] = caseMap.get(lowerQ);
+                continue;
+            }
+            String devowelQ = devowel(lowerQ);
+            if (vowelMap.containsKey(devowelQ)) {
+                ans[i] = vowelMap.get(devowelQ);
+                continue;
+            }
+            ans[i] = "";
+        }
+        return ans;
+    }
+
+    private String devowel(String word) {
+        StringBuilder sb = new StringBuilder();
+        for (char c : word.toCharArray()) {
+            if ("aeiou".indexOf(c) >= 0) sb.append('*');
+            else sb.append(c);
+        }
+        return sb.toString();
+    }
+
     
 }
 
