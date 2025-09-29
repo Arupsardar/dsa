04c633306377f869f7f851cf84232e4ddcec1fa6
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-09-29 22:30:24 +0530

    add new brancsddgfjddddhdgguyvdff

--- Solution/Solution.java
+++ Solution/Solution.java
@@ -7803,6 +7803,24 @@ class Spreadsheet {
         return maxArea;
     }
 
+    public int minScoreTriangulation(int[] values) {
+        int n = values.length;
+        int[][] dp = new int[n][n];
+
+        // dp[i][j] = minimum score triangulation from vertex i to j
+        for (int len = 2; len < n; len++) { // length of sub-polygon
+            for (int i = 0; i + len < n; i++) {
+                int j = i + len;
+                dp[i][j] = Integer.MAX_VALUE;
+                for (int k = i + 1; k < j; k++) {
+                    int cost = dp[i][k] + dp[k][j] + values[i] * values[j] * values[k];
+                    dp[i][j] = Math.min(dp[i][j], cost);
+                }
+            }
+        }
+        return dp[0][n - 1];
+    }
+
 }
 
 class Router {
