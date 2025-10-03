97620c073e71709928f7c8209b08d5c93a833a87
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-10-03 22:27:35 +0530

    add new brancsddgfjddddhdgguyvdff

--- Solution/Solution.java
+++ Solution/Solution.java
@@ -7848,6 +7848,54 @@ class Spreadsheet {
         return ans;
     }
 
+    static class Cell {
+        int row, col, height;
+        Cell(int r, int c, int h) {
+            row = r; col = c; height = h;
+        }
+    }
+    
+    public int trapRainWater(int[][] heightMap) {
+        if (heightMap == null || heightMap.length == 0 || heightMap[0].length == 0) return 0;
+
+        int m = heightMap.length, n = heightMap[0].length;
+        boolean[][] visited = new boolean[m][n];
+        PriorityQueue<Cell> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a.height));
+
+        // Add boundary cells into PQ
+        for (int i = 0; i < m; i++) {
+            pq.offer(new Cell(i, 0, heightMap[i][0]));
+            pq.offer(new Cell(i, n - 1, heightMap[i][n - 1]));
+            visited[i][0] = visited[i][n - 1] = true;
+        }
+        for (int j = 0; j < n; j++) {
+            pq.offer(new Cell(0, j, heightMap[0][j]));
+            pq.offer(new Cell(m - 1, j, heightMap[m - 1][j]));
+            visited[0][j] = visited[m - 1][j] = true;
+        }
+
+        int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
+        int water = 0;
+
+        // Process cells
+        while (!pq.isEmpty()) {
+            Cell cell = pq.poll();
+            for (int[] d : dirs) {
+                int r = cell.row + d[0];
+                int c = cell.col + d[1];
+                if (r >= 0 && r < m && c >= 0 && c < n && !visited[r][c]) {
+                    visited[r][c] = true;
+                    // Water trapped = difference between boundary and neighbor
+                    water += Math.max(0, cell.height - heightMap[r][c]);
+                    // Push updated boundary
+                    pq.offer(new Cell(r, c, Math.max(heightMap[r][c], cell.height)));
+                }
+            }
+        }
+
+        return water;
+    }
+
 }
 
 class Router {
