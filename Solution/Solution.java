a4256f78581cd64f83dee333c21f613af480e9bb
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-10-12 22:06:38 +0530

    add new brancsddgfjddddhdgguyvdffbhjguvjh

--- Solution/Solution.java
+++ Solution/Solution.java
@@ -8078,7 +8078,68 @@ class Spreadsheet {
         return ans;
     }
 
-    
+    static final int MOD = 1_000_000_007, MAX = 31;
+    static final long[] FACT = new long[MAX], INV_FACT = new long[MAX];
+
+    // ✅ Static initialization block for factorials
+    static {
+        FACT[0] = 1;
+        for (int i = 1; i < MAX; i++) FACT[i] = FACT[i - 1] * i % MOD;
+        INV_FACT[MAX - 1] = pow(FACT[MAX - 1], MOD - 2);
+        for (int i = MAX - 1; i > 0; i--) INV_FACT[i - 1] = INV_FACT[i] * i % MOD;
+    }
+
+    // ✅ Modular power function
+    static long pow(long x, int n) {
+        long res = 1;
+        while (n > 0) {
+            if ((n & 1) == 1) res = res * x % MOD;
+            x = x * x % MOD;
+            n >>= 1;
+        }
+        return res;
+    }
+
+    // ✅ Main function to compute magical sum
+    public int magicalSum(int m, int k, int[] nums) {
+        int n = nums.length;
+
+        int[][] pows = new int[n][m + 1];
+        for (int i = 0; i < n; i++) {
+            pows[i][0] = 1;
+            for (int j = 1; j <= m; j++)
+                pows[i][j] = (int) ((long) pows[i][j - 1] * nums[i] % MOD);
+        }
+
+        int[][][][] memo = new int[n + 1][m + 1][m / 2 + 2][k + 2];
+        for (int[][][] a : memo)
+            for (int[][] b : a)
+                for (int[] c : b)
+                    Arrays.fill(c, -1);
+
+        long res = dfs(0, m, 0, k, pows, memo);
+        return (int) (res * FACT[m] % MOD);
+    }
+
+    // ✅ Recursive DP with memoization
+    long dfs(int i, int mLeft, int carry, int kLeft, int[][] pows, int[][][][] memo) {
+        if (kLeft < 0) return 0;
+        if (i == pows.length) {
+            return (mLeft == 0 && Integer.bitCount(carry) == kLeft) ? 1 : 0;
+        }
+        if (memo[i][mLeft][carry][kLeft] != -1)
+            return memo[i][mLeft][carry][kLeft];
+
+        long res = 0;
+        for (int j = 0; j <= mLeft; j++) {
+            int bit = (carry + j) & 1;
+            if (bit <= kLeft) {
+                long val = dfs(i + 1, mLeft - j, (carry + j) >> 1, kLeft - bit, pows, memo);
+                res = (res + val * pows[i][j] % MOD * INV_FACT[j]) % MOD;
+            }
+        }
+        return memo[i][mLeft][carry][kLeft] = (int) res;
+    }
 
 }
 
