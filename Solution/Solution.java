9213d681f342922fa2fc9ac351ad88f2ca3e5b56
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-10-14 22:06:50 +0530

    add new brancsddgfjddddhdgguyvdffbhjguvjh

--- Solution/Solution.java
+++ Solution/Solution.java
@@ -8164,6 +8164,30 @@ class Spreadsheet {
         return ans;
     }
 
+     public boolean hasIncreasingSubarrays(List<Integer> nums, int k) {
+        int n = nums.size();
+        if (n < 2 * k) return false; // Need at least 2*k elements
+
+        boolean[] inc = new boolean[n]; // inc[i] = true if subarray starting at i is strictly increasing
+
+        for (int i = 0; i + k <= n; i++) {
+            boolean ok = true;
+            for (int j = i; j < i + k - 1; j++) {
+                if (nums.get(j) >= nums.get(j + 1)) {
+                    ok = false;
+                    break;
+                }
+            }
+            inc[i] = ok;
+        }
+
+        for (int i = 0; i + 2 * k <= n; i++) {
+            if (inc[i] && inc[i + k]) return true;
+        }
+
+        return false;
+    }
+
 }
 
 class Router {
