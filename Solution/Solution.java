7b56997246c1a08f805a5a0779e725809347fee9
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-10-24 23:07:08 +0530

    add new brancsddgfjddddhdgguyvdffbhjguvjhvhhj

--- Solution/Solution.java
+++ Solution/Solution.java
@@ -8388,6 +8388,28 @@ class Spreadsheet {
         return s.charAt(0) == s.charAt(1);
     }
 
+    public int nextBeautifulNumber(int n) {
+        for (int i = n + 1; ; i++) {
+            if (isBalanced(i)) return i;
+        }
+    }
+
+    private boolean isBalanced(int num) {
+        int[] count = new int[10];
+        int temp = num;
+
+        while (temp > 0) {
+            count[temp % 10]++;
+            temp /= 10;
+        }
+
+        for (int d = 0; d <= 9; d++) {
+            if (count[d] > 0 && count[d] != d) return false;
+        }
+
+        return true;
+    }
+
 }
 
 class Router {
