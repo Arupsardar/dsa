520e114609d48f32cb3c68f61fc1592e9c070735
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-10-28 22:38:32 +0530

    add new brancsddgfjddddhdgguyvdffbhjguvjhvhhj

--- Solution/Solution.java
+++ Solution/Solution.java
@@ -8459,6 +8459,28 @@ class Spreadsheet {
         return result;
     }
 
+    public int countValidSelections(int[] nums) {
+
+        int count = 0, len = nums.length;
+        int sum = 0;
+        for (int num : nums) sum += num;
+
+        int halfSum = 0;
+
+        for (int i = 0; i < len; i++) {
+            halfSum += nums[i];
+            if (nums[i] == 0) {
+
+                if (2 * halfSum == sum) {
+                    count += 2;
+                } else if (Math.abs(sum - 2*halfSum) == 1) {
+                    count++;
+                } 
+            }
+        }
+        return count;
+    }
+
 }
 
 class Router {
