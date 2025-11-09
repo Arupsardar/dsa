86cede92edaae226f2c50f6ec8215543eb86545a
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-11-09 22:25:04 +0530

    add new brancsddgfjddddhdgguyvdffbhjguvjhvhhjbhj

--- Solution/Solution.java
+++ Solution/Solution.java
@@ -8911,6 +8911,20 @@ class Spreadsheet {
             return fp - helper(m);
         }
     }
+
+    public int countOperations(int num1, int num2) {
+        int operations = 0;
+        while (num1 != 0 && num2 != 0) {
+            if (num1 >= num2) {
+                operations += num1 / num2;
+                num1 %= num2;
+            } else {
+                operations += num2 / num1;
+                num2 %= num1;
+            }
+        }
+        return operations;
+    }
     
     
 
