323451964df5e6e3723d0b623d6b90539edf3fb6
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-11-11 23:04:01 +0530

    add new brancsddgfjddddhdgguyvdffbhjguvjhvhhjbhj

--- Solution/Solution.java
+++ Solution/Solution.java
@@ -8925,6 +8925,26 @@ class Spreadsheet {
         }
         return operations;
     }
+
+    public int findMaxForm(String[] strs, int m, int n) {
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (String s : strs) {
+            int zeros = 0, ones = 0;
+            for (char c : s.toCharArray()) {
+                if (c == '0') zeros++;
+                else ones++;
+            }
+
+            // Traverse backwards to avoid overwriting previous states
+            for (int i = m; i >= zeros; i--) {
+                for (int j = n; j >= ones; j--) {
+                    dp[i][j] = Math.max(dp[i][j], dp[i - zeros][j - ones] + 1);
+                }
+            }
+        }
+        return dp[m][n];
+    }
     
     
 
