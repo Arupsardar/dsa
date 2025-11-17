edc2b39d70a14321092c20ca8029836b311e36d5
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-11-17 23:29:05 +0530

    add new brancsddgfjddddhdgguyvdffbhjguvjhvhhjbhj

--- Solution/Solution.java
+++ Solution/Solution.java
@@ -9053,6 +9053,20 @@ class Spreadsheet {
         return (int) count;
     }
 
+    public boolean kLengthApart(int[] nums, int k) {
+        int prev = -1;  // position of last seen 1
+         
+         for (int i = 0; i < nums.length; i++) {
+             if (nums[i] == 1) {
+                 if (prev != -1 && i - prev - 1 < k) {
+                     return false;
+                 }
+                 prev = i;
+             }
+         }
+         return true; 
+     }
+
 
     
     
