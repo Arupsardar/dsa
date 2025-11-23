72bfa71cb949d703f6f29a178d22106cc20d6433
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-11-23 20:26:04 +0530

    add new brancsddgfjddddhdgguyvdffbhjguvjhvhhjbhj

--- Solution/Solution.java
+++ Solution/Solution.java
@@ -9169,6 +9169,21 @@ class Spreadsheet {
         return ans;
     }
 
+    public int maxSumDivThree(int[] nums) {
+       int[] dp = {0, Integer.MIN_VALUE, Integer.MIN_VALUE};
+
+        for (int x : nums) {
+            int[] newdp = dp.clone();
+            for (int r = 0; r < 3; r++) {
+                int nr = (r + x) % 3;
+                newdp[nr] = Math.max(newdp[nr], dp[r] + x);
+            }
+            dp = newdp;
+        }
+
+        return dp[0]; 
+    }
+
 
     
     
