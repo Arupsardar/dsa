2b8597995789f5fecc2a2530433ad79bf47127d3
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-11-25 22:49:42 +0530

    add new brancsddgfjddddhdgguyvdffbhjguvjhvhhjbhj

--- Solution/Solution.java
+++ Solution/Solution.java
@@ -9184,6 +9184,18 @@ class Spreadsheet {
         return dp[0]; 
     }
 
+    public int smallestRepunitDivByK(int k) {
+        if (k % 2 == 0 || k % 5 == 0) return -1;
+        int rem = 0;
+        for (int len = 1; len <= k; len++) {
+            rem = (rem * 10 + 1) % k;
+            if (rem == 0) return len;
+        }
+        return -1;
+    }
+
+
+
 
     
     
