927fe4f517d2fb306494492729ef1fb05b01cc14
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-11-26 22:41:35 +0530

    add new brancsddgfjddddhdgguyvdffbhjguvjhvhhjbhj

--- Solution/Solution.java
+++ Solution/Solution.java
@@ -9194,6 +9194,41 @@ class Spreadsheet {
         return -1;
     }
 
+    public int numberOfPaths(int[][] grid, int k) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        int[][][] dp = new int[m][n][k];
+
+        dp[0][0][grid[0][0] % k] = 1;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                
+                int val = grid[i][j];
+                int modVal = val % k;
+
+                // From top
+                if (i > 0) {
+                    for (int r = 0; r < k; r++) {
+                        int newR = (r + modVal) % k;
+                        dp[i][j][newR] = (dp[i][j][newR] + dp[i-1][j][r]) % MOD;
+                    }
+                }
+
+                // From left
+                if (j > 0) {
+                    for (int r = 0; r < k; r++) {
+                        int newR = (r + modVal) % k;
+                        dp[i][j][newR] = (dp[i][j][newR] + dp[i][j-1][r]) % MOD;
+                    }
+                }
+            }
+        }
+
+        return dp[m-1][n-1][0];
+    }
+
 
 
 
