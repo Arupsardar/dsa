28bcb86d8de55ea9fefdee2e80ffe55f4c141499
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2025-12-02 22:52:44 +0530

    add new brancsddgfjddddhdgguyvdffbhjguvjhvhhjbhj

--- Solution/Solution.java
+++ Solution/Solution.java
@@ -9358,6 +9358,23 @@ class Router {
         }
         return total >= (long) n * T;
     }
+
+
+    public int countTrapezoids(int[][] points) {
+        Map<Integer, Integer> pointNum = new HashMap<>();
+        final int mod = 1000000007;
+        long ans = 0;
+        long sum = 0;
+        for (int[] point : points) {
+            pointNum.put(point[1], pointNum.getOrDefault(point[1], 0) + 1);
+        }
+        for (int pNum : pointNum.values()) {
+            long edge = ((long) pNum * (pNum - 1)) / 2;
+            ans = (ans + edge * sum) % mod;
+            sum = (sum + edge) % mod;
+        }
+        return (int) ans;
+    }
 }
 
 
